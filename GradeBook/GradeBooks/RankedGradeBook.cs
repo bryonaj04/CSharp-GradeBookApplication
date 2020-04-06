75b51eb934692b0f55f23ea423640b2af424de97
Andrew Bryon <andrew.bryon@gilmond.com>
2020-04-06 14:48:18 +0100

    updated gradebook

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -1,4 +1,8 @@
-﻿namespace GradeBook.GradeBooks
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace GradeBook.GradeBooks
 {
     public class RankedGradeBook : BaseGradeBook
     {
@@ -6,5 +10,34 @@
         {
             Type = Enums.GradeBookType.Ranked;
         }
+
+        public override char GetLetterGrade(double averageGrade)
+        {
+
+            if (Students.Count < 5)
+            {
+                throw new InvalidOperationException("Ranked-grading requires a minimum of 5 students to work");
+            }
+
+            foreach (var student in Students)
+            {
+                Students.OrderByDescending(g => g.AverageGrade);
+                var num = Students.Where(f => f.AverageGrade > averageGrade).Count();
+                if (num == 0)
+                    return 'A';
+                else if (num == 1)
+                    return 'B';
+                else if (num == 2)
+                    return 'C';
+                else if (num == 3)
+                    return 'D';
+                else if (num == 4)
+                    return 'D';
+            }
+
+            return 'F';
+
+        }
+
     }
 }
